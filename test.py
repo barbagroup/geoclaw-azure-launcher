90feb5d68691675557a14df7543a7b2401bd624a
Pi-Yueh Chuang <pychuang@gwu.edu>
2019-01-24 04:56:21 -0500

    add test cases

--- test.py
+++ test.py
@@ -9,14 +9,30 @@
 """
 Test the Azure mission launcher.
 """
+import os
+import sys
 from helpers import UserCredential
 from helpers import Mission
 
 
 if __name__ == "__main__":
 
-    user_data = UserCredential(credential_file="./credential.txt")
-    mission = Mission(user_data, "test", 4, ["./test-case-1", "./test-case-2"])
+    # download required files for the test cases
+    script_dir_path = os.path.dirname(os.path.abspath(__file__))
+    sys.path.insert(0, os.path.join(script_dir_path, "test_cases"))
+
+    from download_data import download_all
+
+    download_all()
+
+    # start the mission
+    user_data = UserCredential(
+        credential_file=os.path.join(script_dir_path, "credential.txt"))
+
+    mission = Mission(
+        user_data, "test", 4,
+        [os.path.join(script_dir_path, "test_cases/test_case_1"),
+         os.path.join(script_dir_path, "test_cases/test_case_2")])
 
     mission.start()
     mission.monitor_wait_download()
