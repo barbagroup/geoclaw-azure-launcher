70a54f8192edc475352df8b082433191aade88cf
Pi-Yueh Chuang <pychuang@gwu.edu>
2019-02-04 01:39:35 -0500

    add working prepare_hydro

--- helpers/arcgistools/__init__.py
+++ helpers/arcgistools/__init__.py
@@ -11,6 +11,7 @@ __init__.py
 """
 from helpers.arcgistools.create_folders import create_folders
 from helpers.arcgistools.prepare_topo import prepare_topo
+from helpers.arcgistools.prepare_hydro import prepare_hydro
 from helpers.arcgistools.write_geoclaw_params import print_setrun
 from helpers.arcgistools.write_geoclaw_params import print_roughness
 
--- helpers/arcgistools/prepare_hydro.py
+++ helpers/arcgistools/prepare_hydro.py
@@ -7,37 +7,67 @@
 # Distributed under terms of the MIT license.
 
 """
-WIP
+Prepare hydrological feature rasters.
 """
+import os
+import arcpy
 
-# for i, point in enumerate(points):
 
-#     top = point[1] + domain[0] + 10
-#     bottom = point[1] - domain[1] - 10
-#     left = point[0] - domain[2] - 10
-#     right = point[0] + domain[3] + 10
+def prepare_hydro_single_point(in_feats, point, extent, res, out_dir, ignore=False):
+    """Prepare hydro feature rasters for a single point."""
 
-#     coord = [(left, bottom), (right, bottom), (right, top), (left, top)]
+    top = point[1] + extent[0] + 10
+    bottom = point[1] - extent[1] - 10
+    left = point[0] - extent[2] - 10
+    right = point[0] + extent[3] + 10
+    coord = [(left, bottom), (right, bottom), (right, top), (left, top)]
 
-#     result = arcpy.management.CreateFeatureclass(
-#         arcpy.env.workspace, "square".format(i),
-#         "POLYGON", spatial_reference=3857)
-#     clipper = result[0]
+    result = arcpy.management.CreateFeatureclass(
+        arcpy.env.workspace, "square", "POLYGON", 
+        spatial_reference=3857)
+    clipper = result[0]
 
-#     # Write feature to new feature class
-#     with arcpy.da.InsertCursor(clipper, ['SHAPE@']) as cursor:
-#         cursor.insertRow([coord])
+    # Write feature to new feature class
+    with arcpy.da.InsertCursor(clipper, ['SHAPE@']) as cursor:
+        cursor.insertRow([coord])
 
-#     arcpy.AddMessage(clipper)
-#     for j in range(hydro_layers.rowCount):
-#         indata = hydro_layers.getRow(j).replace("\\", "/")
-#         arcpy.AddMessage(os.path.join(
-#                 arcpy.env.workspace, "clipper{}_{}".format(i, j)))
-#         arcpy.AddMessage(indata)
-#         arcpy.analysis.Clip(
-#             in_features=indata,
-#             clip_features=clipper,
-#             out_feature_class=os.path.join(
-#                 arcpy.env.workspace, "clipper{}_{}".format(i, j)))
+    out_files = []
+    for i, feat in enumerate(in_feats):
 
-#     arcpy.management.Delete(clipper)
+        out_files.append(os.path.join(out_dir, "hydro_{}.asc".format(i)))
+        if os.path.isfile(out_files[-1]):
+            if ignore:
+                continue
+            else:
+                os.remove(out_files[-1])
+
+        arcpy.analysis.Clip(
+            in_features=feat, clip_features=clipper, 
+            out_feature_class="hydro_feat_{}".format(i))
+        
+        arcpy.conversion.FeatureToRaster(
+            "hydro_feat_{}".format(i), "FType", 
+            "hydro_raster_{}".format(i), res)
+        
+        arcpy.conversion.RasterToASCII("hydro_raster_{}".format(i), out_files[-1])
+        
+        arcpy.management.Delete("hydro_feat_{}".format(i))
+        arcpy.management.Delete("hydro_raster_{}".format(i))
+
+    arcpy.management.Delete(clipper)
+    
+    return out_files
+
+def prepare_hydro(in_feats, points, extent, res, output_dirs, ignore=False):
+    """Prepare hydro files for each rupture points by clipping base topo."""
+
+    n_feats = in_feats.rowCount
+    feats = [in_feats.getRow(i).strip("' ") for i in range(n_feats)]
+
+    output_files = []
+
+    for i, point in enumerate(points):
+        temp = prepare_hydro_single_point(feats, point, extent, res, output_dirs[i], ignore)
+        output_files.append(temp)
+
+    return output_files
